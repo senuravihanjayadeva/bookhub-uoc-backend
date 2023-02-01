ae02f0fd1ce234cf3788eafee51170a0886a2791
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-02-01 09:02:47 +0530

    Configure response for default endpoint

--- src/main/java/com/hexagon/bookhub/controller/HomeController.java
+++ src/main/java/com/hexagon/bookhub/controller/HomeController.java
@@ -1,2 +1,17 @@
-package com.hexagon.bookhub.controller;public class HomeController {
+package com.hexagon.bookhub.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "BookHub UOC Backend Service";
+    }
+
 }
